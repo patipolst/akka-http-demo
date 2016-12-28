013303933ce037d58de0d68252db65097a7ada0d
patipol <patipol.s@dotography.com>
2016-12-28 15:42:01 +0700

    add sample custom validator

--- src/main/scala/am/utils/Validator.scala
+++ src/main/scala/am/utils/Validator.scala
@@ -2,8 +2,17 @@ package am.utils
 
 import am.models._
 import com.wix.accord.dsl._
+import com.wix.accord._, ViolationBuilder._
 
-object Validator {
+trait ValidationMethods {
+  def oneOf[ T <: AnyRef ]( options : T* ): Validator[ T ] =
+  new NullSafeValidator[ T ](
+    test    = options.contains,
+    failure = _ -> s"is not one of (${ options.mkString( "," ) })"
+  )
+}
+
+object Validator extends ValidationMethods {
   implicit val addressValidator = validator[Address] { address =>
     address.street is notEmpty
     address.city is notEmpty
